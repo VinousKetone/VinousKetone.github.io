6743aeb935a04c33f65f3fa5098ae45fa4f94842
Vinous Keton <165517041+VinousKetone@users.noreply.github.com>
2024-04-03 12:54:53 -0700

    Add files via upload

---
+++ vccccccc.js
@@ -0,0 +1,12 @@
+function kkk() {
+let c = document.getElementById('ok');
+let a = 12;
+c.innerHTML += 'punheta'
+if(a>=0 && a <12) {
+c.src = 'pexels-cleiton-silva-13468969.jpg'
+}else if(a>=12 && a<18) {
+    c.src = 'pexels-cleiton-silva-13468969.jpg'
+}else if(a>=18 && a<0) {
+    c.src = 'pexels-cleiton-silva-13468969.jpg'
+}
+}
